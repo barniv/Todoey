9fade73b011c90b0e4833ba7cdbfa5a1d8d9c49b
macbook <macbook@macs-macbook-pro.local>
2019-01-27 18:16:41 +0200

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by macbook on 21 Shevat 5779.
+//  Copyright © 5779 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
