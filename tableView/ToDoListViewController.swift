053b9e368dc5384a784d621780d8716f779da057
macbook <macbook@macs-macbook-pro.local>
2019-01-28 18:31:49 +0200

    Data persisted using UserDefaults method

--- tableView/ToDoListViewController.swift
+++ tableView/ToDoListViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
  
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray : [String] = [String]()
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,6 +55,7 @@ class ToDoListViewController: UITableViewController {
         }
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(itemText.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             }
         
